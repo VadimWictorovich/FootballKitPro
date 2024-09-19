b359f6968c2440a76f534eff3c94f386fa8ef177
Vadim <vsadnik199@gmail.com>
2024-09-19 13:53:06 +0300

    ++

--- FootballKitPro/FootballKitPro/GameScene.swift
+++ FootballKitPro/FootballKitPro/GameScene.swift
@@ -10,12 +10,20 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    // TEXTURE
+    var firstBgTexture: SKTexture!
+    var secondBgTexture: SKTexture!
     
-    // NODE
-    var firstLayerBg: SKSpriteNode!
-    var secondLayerBg: SKSpriteNode!
+    // SPRITE NODES
+    var firstBgNode = SKSpriteNode()
+    var secondBgNode = SKSpriteNode()
+    
     var firstPlayerNode: SKSpriteNode!
     var secondPlayerNode: SKSpriteNode!
+    
+    // SPRITE OBJECTS
+    var firstBgObjects = SKNode()
+    var secondBgObjects = SKNode()
     
     override func didMove(to view: SKView) {
         firstPlayerNode = SKSpriteNode(imageNamed: "player1_1")
@@ -24,15 +32,47 @@ class GameScene: SKScene {
         secondPlayerNode = SKSpriteNode(imageNamed: "player2_1")
         secondPlayerNode.position = CGPoint(x: 0, y: 200)
         
-        firstLayerBg = SKSpriteNode(imageNamed: "bgColor1")
-        
-        secondLayerBg = SKSpriteNode(imageNamed: "bg")
-        
-        addChild(firstLayerBg)
-        addChild(secondLayerBg)
+        firstBgTexture = SKTexture(imageNamed: "bgColor1")
+        secondBgTexture = SKTexture(imageNamed: "bg")
+       
+        createObjects()
+        createGame()
         addChild(firstPlayerNode)
         addChild(secondPlayerNode)
     }
+    
+    func createObjects() {
+        self.addChild(firstBgObjects)
+        self.addChild(secondBgObjects)
+    }
+    
+    func createGame() {
+        createBg()
+    }
+    
+    func createBg() {
+        firstBgTexture = SKTexture(imageNamed: "bgColor1")
+        firstBgNode = SKSpriteNode(texture: firstBgTexture)
+        firstBgNode.size.height = self.frame.height
+        firstBgNode.size.width = self.frame.width
+        firstBgNode.zPosition = -1
+        firstBgObjects.addChild(firstBgNode)
+
+        secondBgTexture = SKTexture(imageNamed: "bg")
+        secondBgNode = SKSpriteNode(texture: secondBgTexture)
+        secondBgNode.size.height = self.frame.height
+        secondBgNode.size.width = self.frame.width
+        secondBgNode.zPosition = -1
+        secondBgObjects.addChild(secondBgNode)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
